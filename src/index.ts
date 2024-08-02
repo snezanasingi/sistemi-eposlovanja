6684b52d5d9ce05f5b0cb6fe018faabd5cfdc1a0
snezanasingi <snezana.nastic.22@singimail.rs>
2024-08-02 13:45:56 +0200

    Osnove Express Projekta

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+const port = 4000
+app.listen(port, ()=>{
+    console.log("App started and listening on " +port)
+    
+})
+app.get('/', (req,res)=>{
+
+    res.json({
+        message: "Hello it is me!"
+    })
+})
+
+app.get('*', (req,res)=>{
+
+    res.status(404).json({
+        message: "Not found!"
+    })
+})
